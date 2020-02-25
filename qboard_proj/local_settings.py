e4e63346872ee4568be41dceaa980be4cbf4dccf
Yuto-Aoki <yuuto3039@gmail.com>
2020-02-26 01:52:01 +0900

    Fix Local settings

--- qboard_proj/local_settings.py
+++ qboard_proj/local_settings.py
@@ -1,14 +1,24 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'qboard_proj',
+#         'USER': 'Mitsucari',
+#         'PASSWORD': 'xxxxxxxx',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'qboard_proj',
-        'USER': 'testuser',
-        'PASSWORD': 'xxxxxxxx',
+        'NAME': 'mitsukari_db', #DB名
+        'USER': 'mitsukari',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': 'mitsukari_pass', 
+        'PORT': '5432'
     }
 }
 
