1dff59f945887936a65cf26e99e0631b46e239cd
Yuto-Aoki <yuuto3039@gmail.com>
2020-02-26 04:59:11 +0900

    Fix DATABASE settings.py

--- qboard_proj/settings.py
+++ qboard_proj/settings.py
@@ -81,13 +81,19 @@ WSGI_APPLICATION = 'qboard_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mitsukari_db', #DB名
+#         'USER': 'mitsukari',
+#         'HOST': 'localhost',
+#         'PASSWORD': 'mitsukari_pass', #パスワードがない場合は省略
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mitsukari_db', #DB名
-        'USER': 'mitsukari',
-        'HOST': 'localhost',
-        'PASSWORD': 'mitsukari_pass', #パスワードがない場合は省略
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
