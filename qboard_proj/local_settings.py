c4b61b4f0a8874f8b8aab0e8664baeda85cd9bcb
Yuto-Aoki <yuuto3039@gmail.com>
2020-02-26 05:21:35 +0900

    Change DB

--- qboard_proj/local_settings.py
+++ qboard_proj/local_settings.py
@@ -13,14 +13,20 @@ SECRET_KEY = '0m2_&-(19k4*(rqwn5yx-sn21t&j07q@#u8+=6#u0q6w4bori0'
 #         'PORT': '5432',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mitsukari_db', #DB名
+#         'USER': 'mitsukari',
+#         'HOST': 'localhost',
+#         'PASSWORD': 'mitsukari_pass', 
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mitsukari_db', #DB名
-        'USER': 'mitsukari',
-        'HOST': 'localhost',
-        'PASSWORD': 'mitsukari_pass', 
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
