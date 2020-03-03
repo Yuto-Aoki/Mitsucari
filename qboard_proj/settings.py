bfad0c6e76922614d37f69ac0887016fdcaef0ae
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-04 02:46:22 +0900

    Add .herokuapp

--- qboard_proj/settings.py
+++ qboard_proj/settings.py
@@ -158,7 +158,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*', ".herokuapp.com"]
 
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEBUG = False
 
