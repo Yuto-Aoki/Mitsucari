74640c4f9b67a919dc4514894de83685c536e9db
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-05 01:06:08 +0900

    Add white

--- qboard_proj/settings.py
+++ qboard_proj/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
