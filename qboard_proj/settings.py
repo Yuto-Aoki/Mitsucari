1780850a28f314622bc5703bfd8f3c41f5a69234
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-05 00:17:04 +0900

    load static in index and false

--- qboard_proj/settings.py
+++ qboard_proj/settings.py
@@ -186,4 +186,4 @@ LOGGING = {
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
-    django_heroku.settings(locals())
+    django_heroku.settings(locals(), staticfiles=False)
