06be522da2bd2f26234d092aa782ea8d236d25c1
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-05 01:18:59 +0900

    local only

--- qboard_proj/settings.py
+++ qboard_proj/settings.py
@@ -189,4 +189,4 @@ LOGGING = {
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
-    django_heroku.settings(locals(), staticfiles=False)
+    django_heroku.settings(locals())
