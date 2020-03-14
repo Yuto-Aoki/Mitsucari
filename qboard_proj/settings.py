39e57a078d56987ee1dea4a7ebb650d01a967259
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-15 01:45:26 +0900

    change to static, add staticfilescadd ..

--- qboard_proj/settings.py
+++ qboard_proj/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 #         os.path.join(BASE_DIR, "static"),
 #     ]
 # )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'..', 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'..', 'static'),
 )
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = (
