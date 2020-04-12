638a1f8832d5cd68aab16abf469c8c2211a44c99
Yuto-Aoki <yuuto3039@gmail.com>
2020-04-13 00:37:20 +0900

    Add sheduler

--- qboard_proj/wsgi.py
+++ qboard_proj/wsgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import threading
+import requests
+import time
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qboard_proj.settings')
 
 application = Cling(get_wsgi_application())
+def awake():
+    while True:
+        try:
+            print("Start Awaking")
+            requests.get("https://mitsu-cari.herokuapp.com/")
+            print("End")
+        except:
+            print("error")
+        time.sleep(1680)
+
+t = threading.Thread(target=awake)
+t.start()
